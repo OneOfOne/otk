36a30ee948ad3375a57a3d2aa42b490b83f36f63
OneOfOne <oneofone@gmail.com>
2020-07-14 14:08:22 -0500

    matchemail -> validemail

--- reeeee.go
+++ reeeee.go
@@ -10,11 +10,8 @@ var (
 	emailsOnce sync.Once
 )
 
-// Email returns a copy
-func Email() *regexp.Regexp { return emails.Copy() }
-
-// MatchEmail matches against the main regexp, not recommended for high concurrency.
-func MatchEmail(email string) bool {
+// ValidEmail checks the given email for validity
+func ValidEmail(email string) bool {
 	emailsOnce.Do(func() {
 		// source http://emailregex.com/ javascript regex
 		emails = regexp.MustCompile(`^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$`)
