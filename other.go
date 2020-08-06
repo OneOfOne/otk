01f3e4fe2893221f7068341e89955163b95bdcf7
OneOfOne <oneofone@gmail.com>
2020-08-05 20:08:45 -0500

    TryParseTime

--- other.go
+++ other.go
@@ -1,5 +1,10 @@
 package otk
 
+import (
+	"strconv"
+	"time"
+)
+
 // UniqueSlice returns all unique keys in `in` by modifying it in place
 func UniqueSlice(in []string) (out []string) {
 	set := make(Set, len(in))
@@ -12,4 +17,41 @@ func UniqueSlice(in []string) (out []string) {
 	}
 
 	return
+}
+
+// TryParseTime will try to parse input in all provided go time formats.
+// Accepts: U for unix ts, UN for unix nano.
+func TryParseTime(ts string, layouts []string, tz *time.Location) time.Time {
+	const ms = 10000000000
+	var (
+		t   time.Time
+		err error
+	)
+
+	if tz == nil {
+		tz = time.UTC
+	}
+
+	for _, l := range layouts {
+		if l == "U" || l == "UN" {
+			var n int64
+			if n, err = strconv.ParseInt(ts, 10, 64); err != nil {
+				continue
+			}
+			if l == "U" {
+				if n > ms { // javascript ts in ms
+					n = n / 1000
+				}
+				t = time.Unix(n, 0)
+			} else {
+				t = time.Unix(0, n)
+			}
+			return t.In(tz)
+		}
+
+		if t, err = time.ParseInLocation(l, ts, tz); err == nil {
+			return t
+		}
+	}
+	return time.Time{}
 }
