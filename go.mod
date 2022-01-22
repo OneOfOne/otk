d05c82f0985d9450291db1bec412c56eb2b7f364
OneOfOne <oneofone@gmail.com>
2022-01-22 17:37:24 -0600

    prealloc utils

--- go.mod
+++ go.mod
@@ -1,9 +1,9 @@
 module go.oneofone.dev/otk
 
-go 1.15
+go 1.18
 
 require (
 	go.oneofone.dev/sets v1.0.9-0.20210819184551-3b382069c88a
-	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d
+	golang.org/x/image v0.0.0-20211028202545-6944b10bf410
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
 )
---
+++ prealloc_go118.go
@@ -0,0 +1,55 @@
+//go:build go118
+// +build go118
+
+package otk
+
+import "sync"
+
+type ptrGen[T any] struct {
+	raw []T
+	cap int
+}
+
+func (p *ptrGen[T]) next() (v *T) {
+	if len(p.raw) == 0 {
+		p.raw = make([]T, p.cap)
+	}
+
+	v = &p.raw[0]
+	p.raw = p.raw[1:]
+
+	return
+}
+
+func PtrGen[T any](cap int, safe bool) func() *T {
+	if cap < 1 {
+		cap = 64
+	}
+
+	p := ptrGen[T]{cap: cap}
+
+	if !safe {
+		return p.next
+	}
+
+	var mux sync.Mutex
+	return func() *T {
+		mux.Lock()
+		v := p.next()
+		mux.Unlock()
+		return v
+	}
+}
+
+func ValuesToPtrs[T any](vals []T, noCopy bool) []*T {
+	out := make([]*T, 0, len(vals))
+	for i := range vals {
+		v := &vals[i]
+		if noCopy {
+			cp := *v
+			v = &cp
+		}
+		out = append(out, v)
+	}
+	return out
+}
