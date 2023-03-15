6d663aa717aab371b28362d0e3ae9b9fd6886458
OneOfOne <oneofone@gmail.com>
2023-03-15 12:16:35 -0500

    redo sem

--- sem.go
+++ sem.go
@@ -1,16 +1,5 @@
 package otk
 
-import (
-	"errors"
-	"sync"
-)
-
-var (
-	ErrClosedSem      = errors.New("sem is already closed")
-	ErrNegativeNumber = errors.New("n < 1")
-	ErrSemIsFull      = errors.New("sem is full")
-)
-
 func NewSem(size int) *Sem {
 	if size == 0 {
 		size = 1
@@ -21,66 +10,20 @@ func NewSem(size int) *Sem {
 
 type Sem struct {
 	ch chan struct{}
-	m  sync.Mutex
 }
 
-func (s *Sem) Acquire(n int) error {
-	if n < 1 {
-		return ErrNegativeNumber
-	}
-
-	var e struct{}
-	for i := 0; i < n; i++ {
-		s.m.Lock()
-		if s.ch == nil {
-			s.m.Unlock()
-			return ErrClosedSem
-		}
-
-		s.ch <- e
-		s.m.Unlock()
-	}
-
-	return nil
+func (s *Sem) Acquire() {
+	s.ch <- struct{}{}
 }
 
-func (s *Sem) Release(n int) error {
-	if n < 1 {
-		return ErrNegativeNumber
-	}
-
-	for i := 0; i < n; i++ {
-		s.m.Lock()
-		if s.ch == nil {
-			s.m.Unlock()
-			return ErrClosedSem
-		}
-		<-s.ch
-		s.m.Unlock()
-	}
-
-	return nil
+func (s *Sem) Release() {
+	<-s.ch
 }
 
-func (s *Sem) Go(fn func()) error {
-	if err := s.Acquire(1); err != nil {
-		return err
-	}
+func (s *Sem) Go(fn func()) {
+	s.Acquire()
 	go func() {
-		defer s.Release(1)
+		defer s.Release()
 		fn()
 	}()
-
-	return nil
-}
-
-func (s *Sem) Close() error {
-	s.m.Lock()
-	defer s.m.Unlock()
-	if s.ch == nil {
-		return ErrClosedSem
-	}
-	close(s.ch)
-	s.ch = nil
-	return nil
 }
